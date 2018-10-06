2ccd380682197366e6c591ba7b469c850cf5b1ac
Adi <adityarajguru.ar@gmail.com>
2018-10-06 13:00:29 -0500

    Completed Parsing from URL to a object in parser.py

---
+++ parser.py
@@ -0,0 +1,35 @@
+import nltk
+from newspaper import Article
+from bs4 import BeautifulSoup
+
+
+
+def fetch_data(url):
+    
+    
+    url_webpage = url
+    article_cnn=Article(url,language="en")
+    article_cnn.download()
+    article_cnn.parse()
+    #article_cnn.nlp()
+    print("The name of the Article :"+article_cnn.title)
+    print("Body:"+article_cnn.text)
+    #print("Summary:"+article_cnn.summary)
+    #print("Keywords:")
+    for keyword in article_cnn.keywords:
+        print(keyword)
+
+
+
+fetch_data('https://www.bbc.com/news/uk-45768848')
+#quote_page = 'https://www.cnn.com/2018/10/06/politics/kavanaugh-final-confirmation-vote/index.html'
+#page = urllib2.urlopen(quote_page)
+#oup=BeautifulSoup(page,'html.parser')
+
+#headline_box = soup.find('h1',attrs={'class':'pg-headline'})
+#headline_name = headline_box.text.strip()
+#print(headline_name)
+#try:
+   # import urllib.request as urllib2
+#except ImportError:
+ #   import urllib2
